88ce029797948c1f255d953b14d7633bbeefbd21
Mohamed Oussama Benkaddour <mohamedoussama2k05@gmail.com>
2026-03-06 21:30:58 +0000

    Create exercice5.js

---
+++ exercice5.js
@@ -0,0 +1,13 @@
+let a = "123";
+let b = "45.6";
+let c = "hello";
+
+console.log("Number(a) :", Number(a));
+console.log("Number(b) :", Number(b));
+console.log("Number(c) :", Number(c));
+
+let d = 10;
+let e = true;
+
+console.log("String(d) :", String(d));
+console.log("String(e) :", String(e));
